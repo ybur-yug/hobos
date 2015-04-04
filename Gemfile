4e13916363782fb1b2c731719034f10fd6c50a5c
Robert H Grayson II <bobbygrayson@gmail.com>
2015-04-04 17:58:44 -0400

    setup as sinatra app

---
+++ Gemfile
@@ -0,0 +1,10 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem 'mechanize'
+gem 'sinatra'
+
+group :test, :development do
+  gem 'rspec'
+end
+
--- lib/hobos.rb
+++ lib/hobos.rb
@@ -1,40 +1,45 @@
 require 'mechanize'
+require 'sinatra'
 
-module Hobos
+get '/hobos' do
+  @params = JSON.parse request.body
+  binding.pry
+  module Hobos
 
-  class HoboAPI
-    def initialize
-      @browser = Browser.new.browser
-    end
+    class HoboAPI
+      def initialize
+        @browser = Browser.new.browser
+      end
 
-    def hobo uid
-      begin
-        clean_name(@browser.get("http://www.e-hobo.com/hoboes/#{uid}").at('span').children.last.to_s)
-      rescue
-        "{ 'error': { 'message': 'bad UID' } }"
+      def hobo uid
+        begin
+          clean_name(@browser.get("http://www.e-hobo.com/hoboes/#{uid}").at('span').children.last.to_s)
+        rescue
+          "{ 'error': { 'message': 'bad UID' } }"
+        end
+      end
+
+      private
+      def clean_name hobo_name
+        hobo_name.gsub!(/\d/, '')
+        hobo_name.gsub!('#', '')
+        hobo_name.gsub!(': ', '')
+        hobo_name
       end
     end
 
-    private
-    def clean_name hobo_name
-      hobo_name.gsub!(/\d/, '')
-      hobo_name.gsub!('#', '')
-      hobo_name.gsub!(': ', '')
-      hobo_name
+    class Browser
+      attr_accessor :browser
+      attr_accessor :hobos_list
+
+      def initialize
+        @browser = Mechanize.new
+        @hobos_list = @browser.get("http://www.e-hobo.com/hoboes/list/")
+      end
     end
+
   end
 
-  class Browser
-    attr_accessor :browser
-    attr_accessor :hobos_list
-
-    def initialize
-      @browser = Mechanize.new
-      @hobos_list = @browser.get("http://www.e-hobo.com/hoboes/list/")
-    end
-  end
-
+  api = Hobos::HoboAPI.new
+  api.hobo 2
 end
-
-api = Hobos::HoboAPI.new
-puts api.hobo 9999
