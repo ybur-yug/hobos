ff9bf7b0268210801be001e9801737e45e2c3598
Robert H Grayson II <bobbygrayson@gmail.com>
2015-04-04 18:07:17 -0400

    remove sinatra for modularity

--- lib/hobos.rb
+++ lib/hobos.rb
@@ -1,45 +1,39 @@
 require 'mechanize'
-require 'sinatra'
+module Hobos
 
-get '/hobos' do
-  @params = JSON.parse request.body
-  binding.pry
-  module Hobos
+  class HoboAPI
+    def initialize
+      @browser = Browser.new.browser
+    end
 
-    class HoboAPI
-      def initialize
-        @browser = Browser.new.browser
-      end
-
-      def hobo uid
-        begin
-          clean_name(@browser.get("http://www.e-hobo.com/hoboes/#{uid}").at('span').children.last.to_s)
-        rescue
-          "{ 'error': { 'message': 'bad UID' } }"
-        end
-      end
-
-      private
-      def clean_name hobo_name
-        hobo_name.gsub!(/\d/, '')
-        hobo_name.gsub!('#', '')
-        hobo_name.gsub!(': ', '')
-        hobo_name
+    def hobo uid
+      begin
+        clean_name(@browser.get("http://www.e-hobo.com/hoboes/#{uid}").at('span').children.last.to_s)
+      rescue
+        "{ 'error': { 'message': 'bad UID' } }"
       end
     end
 
-    class Browser
-      attr_accessor :browser
-      attr_accessor :hobos_list
-
-      def initialize
-        @browser = Mechanize.new
-        @hobos_list = @browser.get("http://www.e-hobo.com/hoboes/list/")
-      end
+    private
+    def clean_name hobo_name
+      hobo_name.gsub!(/\d/, '')
+      hobo_name.gsub!('#', '')
+      hobo_name.gsub!(': ', '')
+      hobo_name
     end
-
   end
 
-  api = Hobos::HoboAPI.new
-  api.hobo @params['uid']
+  class Browser
+    attr_accessor :browser
+    attr_accessor :hobos_list
+
+    def initialize
+      @browser = Mechanize.new
+      @hobos_list = @browser.get("http://www.e-hobo.com/hoboes/list/")
+    end
+  end
+
 end
+
+api = Hobos::HoboAPI.new
+api.hobo @params['uid']
